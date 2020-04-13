25e19187cfa975e12c945823142678c62cb9937b
Divish1032 <kumar.divansh1996@gmail.com>
2020-04-13 19:55:18 +0530

    G

--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
-import { Notes } from '../models/Notes';
+import { Notes } from '../models/notes';
 import { NotesService } from '../services/notes.service'
 import { ToastrService } from 'ngx-toastr';
 
